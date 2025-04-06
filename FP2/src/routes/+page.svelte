a133f113d7d66e4833523191c19505d5c5440b6d
Autumn <arartist@mit.edu>
2025-04-06 06:56:41 -0400

    First draft of 3d corporate graph

--- FP2/src/routes/+page.svelte
+++ FP2/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<iframe src="/index.html" style="width: 100%; height: 100vh; border: none;"
+></iframe>
